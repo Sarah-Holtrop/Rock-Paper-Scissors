f5c2d63bed2ec544c8d500c6d8d268caf5a556f1
Sarah-Opalenik <sarahmo93@gmail.com>
2019-06-12 14:19:15 -0600

    shows p/c choice

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 
 function play(playerChoice) {
+
   let index = Math.floor(Math.random() * 3);
 
   let computerOptions = ["rock", "paper", "scissors"];
@@ -9,20 +10,20 @@ function play(playerChoice) {
   console.log("Computer Played " + computerChoice)
 
   if (playerChoice == computerChoice) { //pRPS==cRPS
-    console.log("Tie")
   } else if (playerChoice == "paper" && computerChoice == "rock") {
-    console.log("Player 1 Wins")
   } else if (playerChoice == "scissors" && computerChoice == "rock") {
-    console.log("Computer wins!")
   } else if (playerChoice == "rock" && computerChoice == "paper") {
-    console.log("Computer wins!")
   } else if (playerChoice == "scissors" && computerChoice == "paper") {
-    console.log("Player 1 wins!")
   } else if (playerChoice == "rock" && computerChoice == "scissors") {
-    console.log("Player 1 wins!")
   } else if (playerChoice == "paper" && computerChoice == "scissors") {
-    console.log("Computer wins!")
   }
+  update(playerChoice, computerChoice)
+}
+
+function update(playerChoice, computerChoice) {
+  document.getElementById("player-rps").innerHTML = playerChoice
+  document.getElementById("computer-rps").innerHTML = computerChoice
+
 }
 
 
